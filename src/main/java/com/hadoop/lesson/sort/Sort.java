e3cddd69023a6dcad22a8c8edb4980707d562739
jackgdn <li_zhong_yao@foxmail.com>
2025-05-07 14:01:39 +0800

    init

---
+++ src/main/java/com/hadoop/lesson/sort/Sort.java
@@ -0,0 +1,71 @@
+package com.hadoop.lesson.sort;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class Sort {
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.println("Usage: SortDriver <input path> <output path>");
+            System.exit(-1);
+        }
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "Sort Numbers");
+        job.setJarByClass(Sort.class);
+        job.setMapperClass(SortMapper.class);
+        job.setReducerClass(SortReducer.class);
+        job.setMapOutputKeyClass(NullWritable.class);
+        job.setMapOutputValueClass(IntWritable.class);
+        job.setOutputKeyClass(IntWritable.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
+
+class SortMapper extends Mapper<LongWritable, Text, NullWritable, IntWritable> {
+    private IntWritable num = new IntWritable();
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        try {
+            int number = Integer.parseInt(value.toString());
+            num.set(number);
+            context.write(NullWritable.get(), num);
+        } catch (NumberFormatException e) {
+            System.err.println("Invalid input: " + value.toString());
+        }
+    }
+}
+
+class SortReducer extends Reducer<NullWritable, IntWritable, IntWritable, IntWritable> {
+    private List<Integer> numbers = new ArrayList<>();
+
+    @Override
+    protected void reduce(NullWritable key, Iterable<IntWritable> values, Context context)
+            throws IOException, InterruptedException {
+        for (IntWritable value : values) {
+            numbers.add(value.get());
+        }
+        Collections.sort(numbers);
+        int index = 1;
+        for (int num : numbers) {
+            context.write(new IntWritable(index++), new IntWritable(num));
+        }
+    }
+}
